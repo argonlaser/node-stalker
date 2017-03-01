dc2826193ad70340b8cdc5e17c4fa908f003478b
Venkata krishna Sundararajan <vkvenkat94@gmail.com>
2017-03-01 22:11:54 +0530

    adds more examples
    Signed-off-by: Venkata krishna Sundararajan <vkvenkat94@gmail.com>

--- examples/example.js
+++ examples/example.js
@@ -5,6 +5,9 @@ function example () {
    * Some lines of code
    */
   stalk(1, 2, 3, 4)
+  stalk()
+  stalk(JSON.stringify({a:1, b:2}), [2,3,4,5,6])
+  stalk('hello world');
   /*
    * Some lines of code
    */
